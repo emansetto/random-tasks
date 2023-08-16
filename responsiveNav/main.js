4bca6dd85a1871fd2c306dea303def9297bf2e67
DESKTOP-E831T06\Eman <emansetto@gmail.com>
2023-08-16 23:10:06 +0300

    finishing

---
+++ responsiveNav/main.js
@@ -0,0 +1,18 @@
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click' , ()=>{})
+{
+const visibility = primaryNav.getAttribute('data-visible');
+if(visibility==="true")
+{
+   primaryNav.setAttribute('data-visible', true);
+   navToggle.setAttribute('aria-expanded',true);
+}
+else
+{
+    primaryNav.setAttribute('data-visible', false);
+    navToggle.setAttribute('aria-expanded',false);
+
+}
+}
