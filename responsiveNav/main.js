105b0650d74626bf398f4d43d29c54004e8f3638
DESKTOP-E831T06\Eman <emansetto@gmail.com>
2023-08-17 00:44:19 +0300

    add missed files

--- responsiveNav/main.js
+++ responsiveNav/main.js
@@ -6,13 +6,13 @@ navToggle.addEventListener('click' , ()=>{})
 const visibility = primaryNav.getAttribute('data-visible');
 if(visibility==="true")
 {
-   primaryNav.setAttribute('data-visible', true);
-   navToggle.setAttribute('aria-expanded',true);
+   primaryNav.setAttribute('data-visible', "true");
+   navToggle.setAttribute('aria-expanded',"true");
 }
 else
 {
-    primaryNav.setAttribute('data-visible', false);
-    navToggle.setAttribute('aria-expanded',false);
+    primaryNav.setAttribute('data-visible', "false");
+    navToggle.setAttribute('aria-expanded', "false");
 
 }
 }
